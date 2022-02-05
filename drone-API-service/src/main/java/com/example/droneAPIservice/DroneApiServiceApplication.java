ee6fb1aa72227598133bab540eb66dcc5a23ecd8
reubie <r.nguyo27@gmail.com>
2022-02-05 19:13:12 +0300

    add a modelmapper function

--- drone-API-service/src/main/java/com/example/droneAPIservice/DroneApiServiceApplication.java
+++ drone-API-service/src/main/java/com/example/droneAPIservice/DroneApiServiceApplication.java
@@ -1,10 +1,19 @@
 package com.example.droneAPIservice;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DroneApiServiceApplication {
+
+	@Bean
+	public ModelMapper modelMapper() {
+		ModelMapper mapper = new ModelMapper();
+		mapper.getConfiguration().setSkipNullEnabled(true);
+		return mapper;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DroneApiServiceApplication.class, args);
